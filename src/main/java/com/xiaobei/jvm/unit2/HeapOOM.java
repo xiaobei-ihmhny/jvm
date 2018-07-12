1d42e7ab2dc97577c54b1fc1cef0ad2d5264fd97
dengqh <dqh2827@163.com>
2018-07-13 07:00:47 +0800

    构建项目

---
+++ src/main/java/com/xiaobei/jvm/unit2/HeapOOM.java
@@ -0,0 +1,11 @@
+package com.xiaobei.jvm.unit2;
+
+/**
+ * @author: xiaobei
+ * @createTime: 2018-07-13 6:58
+ * @since: JDK 1.8
+ * @description: Java堆内存溢出异常测试
+ */
+public class HeapOOM {
+
+}
---
+++ src/main/java/com/xiaobei/jvm/unit2/JavaStackSOF.java
@@ -0,0 +1,11 @@
+package com.xiaobei.jvm.unit2;
+
+/**
+ * @author: xiaobei
+ * @createTime: 2018-07-13 6:57
+ * @since: JDK 1.8
+ * @description: 虚拟机栈和本地方法栈OOM测试
+ */
+public class JavaStackSOF {
+
+}
